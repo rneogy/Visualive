2329241361c3c195458cc79b23b92d4dd61495f3
Rupayan Neogy <rneogy@mit.edu>
2019-04-21 19:54:57 -0400

    rerendering workign

--- client/src/components/Vis.js
+++ client/src/components/Vis.js
@@ -6,7 +6,9 @@ class Vis extends React.Component {
   constructor(props) {
     super(props);
     this.socket = io("http://localhost:3000");
-    this.selectedCountry = "";
+    this.state = {
+      selectedCountry: ""
+    };
   }
 
   componentDidMount() {
@@ -55,57 +57,11 @@ class Vis extends React.Component {
   drawChart() {
     d3.csv("/data/income.csv").then(data => {
       this.data = data;
-      let countryNames = d3.map(data, d => d.country).keys()
-
-      function onChange() {
-        let countryValue = d3.select('select').property('value')
-        console.log(countryValue);
-      }
-
-      console.log(countryNames);
-      let dropdown = d3.select("#vis").append("select").attr("id", "country-dropdown");
-      dropdown
-        .selectAll("option")
-        .data(countryNames)
-        .enter()
-        .append("option")
-        .attr("value", d => d)
-        .text(d => d)
-        .on("change", onChange);
+      const countryNames = d3.map(data, d => d.country).keys();
 
       const w = this.divElement.clientWidth;
       const h = document.documentElement.clientHeight;
-      const country = data[0];
-      const countryData = Object.keys(country).reduce((res, k) => {
-        if (k !== "country") {
-          res.push({ year: +k, income: +country[k] });
-        }
-        return res;
-      }, []);
-
-      const maxIncome = d3.max(countryData, d => d.income);
-
-      this.x = d3
-        .scaleLinear()
-        .domain([1800, 2040])
-        .range([0.05 * w, 0.95 * w]);
-
-      this.t = this.x;
-
-      this.xAxis = d3
-        .axisBottom()
-        .scale(this.t)
-        .tickFormat(d3.format("d"));
-
       this.dx = w / (2040 - 1800);
-
-      this.y = d3
-        .scaleLinear()
-        .domain([0, maxIncome])
-        .range([0.95 * h, 0.05 * h])
-        .nice();
-
-      this.yAxis = d3.axisLeft().scale(this.y);
 
       const svg = d3
         .select("#vis")
@@ -116,13 +72,26 @@ class Vis extends React.Component {
       svg
         .append("g")
         .attr("id", "xAxis")
-        .attr("transform", `translate(0,${0.96 * h})`)
-        .call(this.xAxis);
+        .attr("transform", `translate(0,${0.96 * h})`);
 
       svg
         .append("g")
-        .attr("transform", `translate(${0.04 * w}, 0)`)
-        .call(this.yAxis);
+        .attr("id", "yAxis")
+        .attr("transform", `translate(${0.04 * w}, 0)`);
+
+      this.x = d3
+        .scaleLinear()
+        .domain([1800, 2040])
+        .range([0.05 * w, 0.95 * w]);
+
+      this.xAxis = d3
+        .axisBottom()
+        .scale(this.x)
+        .tickFormat(d3.format("d"));
+
+      this.y = d3.scaleLinear().range([0.95 * h, 0.05 * h]);
+
+      this.yAxis = d3.axisLeft();
 
       const zoom = d3
         .zoom()
@@ -130,7 +99,7 @@ class Vis extends React.Component {
         .translateExtent([[-100, 0], [w + 100, 0]])
         .on("zoom", this.zoomed);
 
-      d3.select("svg").call(zoom);
+      svg.call(zoom);
 
       // Clipping
       svg
@@ -138,34 +107,105 @@ class Vis extends React.Component {
         .append("clipPath")
         .attr("id", "clip")
         .append("rect")
-        .attr("x", 0.05*w)
-        .attr("y", 0.05*h)
-        .attr("width", 0.9*w)
-        .attr("height", 0.9*h);
+        .attr("x", 0.05 * w)
+        .attr("y", 0.05 * h)
+        .attr("width", 0.9 * w)
+        .attr("height", 0.9 * h);
 
       const main = svg
         .append("g")
         .attr("class", "main")
         .attr("clip-path", "url(#clip)");
 
-      main
-        .selectAll("rect")
-        .data(countryData)
+      const renderChart = () => {
+        this.setState({ selectedCountry: this.dropdown.property("value") });
+        console.log("rendering " + this.state.selectedCountry);
+        const rawCountryData = data.find(
+          d => d.country === this.state.selectedCountry
+        );
+        const countryData = Object.keys(rawCountryData).reduce((res, k) => {
+          if (k !== "country") {
+            res.push({ year: +k, income: +rawCountryData[k] });
+          }
+          return res;
+        }, []);
+
+        const maxIncome = d3.max(countryData, d => d.income);
+
+        this.t = this.x;
+        this.xAxis.scale(this.t);
+
+        this.y.domain([0, maxIncome]).nice();
+
+        this.yAxis.scale(this.y);
+
+        d3.select("#xAxis")
+          .transition()
+          .duration(2000)
+          .call(this.xAxis);
+
+        d3.select("#yAxis")
+          .transition()
+          .duration(2000)
+          .call(this.yAxis);
+
+        const bars = main.selectAll("rect.bar").data(countryData);
+
+        bars
+          .enter()
+          .append("rect")
+          .classed("bar", true)
+          .attr("x", d => {
+            return this.x(d.year);
+          })
+          .attr("y", d => {
+            return this.y(d.income)-h/2;
+          })
+          .attr("width", this.dx / 2)
+          .attr("height", d => 0.95 * h - this.y(d.income))
+          .attr("fill", "green")
+          .attr("id", (_, i) => "b-" + i)
+          .attr("opacity", 0)
+          .on("mouseover", this.onMouseOver)
+          .on("mouseout", this.onMouseOut)
+          .transition()
+          .delay((_,i) => i*3)
+          .duration(2000)
+          .attr("opacity", 1)
+          .attr("y", d => {
+            return this.y(d.income);
+          });
+
+        bars
+          .transition()
+          .duration(2000)
+          .attr("x", d => {
+            return this.x(d.year);
+          })
+          .attr("y", d => {
+            return this.y(d.income);
+          })
+          .attr("width", this.dx / 2)
+          .attr("height", d => 0.95 * h - this.y(d.income))
+          .attr("fill", "green")
+          .attr("id", (_, i) => "b-" + i);
+      };
+
+      this.dropdown = d3
+        .select("#vis")
+        .append("select")
+        .attr("id", "country-dropdown")
+        .on("change", renderChart);
+
+      this.dropdown
+        .selectAll("option")
+        .data(countryNames)
         .enter()
-        .append("rect")
-        .classed("bar", true)
-        .attr("x", d => {
-          return this.x(d.year);
-        })
-        .attr("y", d => {
-          return this.y(d.income);
-        })
-        .attr("width", this.dx / 2)
-        .attr("height", d => 0.95 * h - this.y(d.income))
-        .attr("fill", "green")
-        .attr("id", (_, i) => "b-" + i)
-        .on("mouseover", this.onMouseOver)
-        .on("mouseout", this.onMouseOut);
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
+
+      renderChart();
     });
   }
 
