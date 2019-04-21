e18af72d65cc4f90a90885f87e3b15ae788bbf06
Rupayan Neogy <rneogy@mit.edu>
2019-04-21 19:00:21 -0400

    axes and clipping and better sizing

--- client/src/components/Vis.js
+++ client/src/components/Vis.js
@@ -12,19 +12,15 @@ class Vis extends React.Component {
     this.drawChart();
 
     this.socket.on("highlight", i => {
-      console.log("highlighting " + i);
       d3.select("#b-" + i)
         .attr("fill", "red")
         .attr("width", this.dx);
-      // .attr("x", this.dx * i - this.dx / 4);
     });
 
     this.socket.on("unhighlight", i => {
-      console.log("unhighlighting " + i);
       d3.select("#b-" + i)
         .attr("fill", "green")
-        .attr("width", this.dx / 2)
-        .attr("x", this.dx * i);
+        .attr("width", this.dx / 2);
     });
   }
 
@@ -32,13 +28,12 @@ class Vis extends React.Component {
     d3.select("#b-" + i)
       .attr("fill", "orange")
       .attr("width", this.dx);
-    // .attr("x", this.dx * i - this.dx / 4);
 
     // Specify where to put label of text
     d3.select("svg")
       .append("text")
       .attr("id", "t-" + i)
-      .attr("x", this.dx * i)
+      .attr("x", this.t(d.year))
       .attr("y", this.y(d.income) - 20)
       .attr("text-anchor", "middle")
       .text(d.year + ": " + d.income);
@@ -50,7 +45,6 @@ class Vis extends React.Component {
     d3.select("#b-" + i)
       .attr("fill", "green")
       .attr("width", this.dx / 2);
-    // .attr("x", this.dx * i);
 
     d3.select("#t-" + i).remove(); // Remove text location
 
@@ -75,15 +69,24 @@ class Vis extends React.Component {
       this.x = d3
         .scaleLinear()
         .domain([1800, 2040])
-        .range([0, w]);
+        .range([0.05 * w, 0.95 * w]);
+
+      this.t = this.x;
+
+      this.xAxis = d3
+        .axisBottom()
+        .scale(this.t)
+        .tickFormat(d3.format("d"));
 
       this.dx = w / (2040 - 1800);
 
       this.y = d3
         .scaleLinear()
         .domain([0, maxIncome])
-        .range([h, 0])
+        .range([0.95 * h, 0.05 * h])
         .nice();
+
+      this.yAxis = d3.axisLeft().scale(this.y);
 
       const svg = d3
         .select("#vis")
@@ -92,10 +95,46 @@ class Vis extends React.Component {
         .attr("height", h);
 
       svg
+        .append("g")
+        .attr("id", "xAxis")
+        .attr("transform", `translate(0,${0.96 * h})`)
+        .call(this.xAxis);
+
+      svg
+        .append("g")
+        .attr("transform", `translate(${0.04 * w}, 0)`)
+        .call(this.yAxis);
+
+      const zoom = d3
+        .zoom()
+        .scaleExtent([0.8, 20])
+        .translateExtent([[-100, 0], [w + 100, 0]])
+        .on("zoom", this.zoomed);
+
+      d3.select("svg").call(zoom);
+
+      // Clipping
+      svg
+        .append("defs")
+        .append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("x", 0.05*w)
+        .attr("y", 0.05*h)
+        .attr("width", 0.9*w)
+        .attr("height", 0.9*h);
+
+      const main = svg
+        .append("g")
+        .attr("class", "main")
+        .attr("clip-path", "url(#clip)");
+
+      main
         .selectAll("rect")
         .data(countryData)
         .enter()
         .append("rect")
+        .classed("bar", true)
         .attr("x", d => {
           return this.x(d.year);
         })
@@ -103,24 +142,21 @@ class Vis extends React.Component {
           return this.y(d.income);
         })
         .attr("width", this.dx / 2)
-        .attr("height", d => h - this.y(d.income))
+        .attr("height", d => 0.95 * h - this.y(d.income))
         .attr("fill", "green")
         .attr("id", (_, i) => "b-" + i)
         .on("mouseover", this.onMouseOver)
         .on("mouseout", this.onMouseOut);
-
-      const zoom = d3.zoom()
-                      .scaleExtent([0.5, 20])
-                      .translateExtent([[-100, -50], [w + 100, h]])
-                      .on("zoom", zoomed);
-
-      d3.select("svg").call(zoom);
-
-      function zoomed() {
-        d3.selectAll("rect").attr("transform", d3.event.transform);
-      }
     });
   }
+
+  zoomed = () => {
+    this.t = d3.event.transform.rescaleX(this.x);
+    d3.selectAll("rect.bar").attr("x", d => {
+      return this.t(d.year);
+    });
+    d3.select("#xAxis").call(this.xAxis.scale(this.t));
+  };
 
   render() {
     return <div id="vis" ref={divElement => (this.divElement = divElement)} />;
